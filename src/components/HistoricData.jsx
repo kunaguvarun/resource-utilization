bacb70e4912312a7a91ef9ac06fc6339ef8122e1
Kunagu Varun Kumar <kunaguvarun@gmail.com>
2019-07-09 23:00:16 -0400

    setup header and dropdown

--- src/components/HistoricData.jsx
+++ src/components/HistoricData.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { Row, Col, Container } from "reactstrap";
 
 class HistoricData extends Component {
   render() {
-    return <div />;
+    return (
+      <Container>
+        <Row>
+          <Col sm="3">
+            <h2>Historic Data</h2>
+          </Col>
+          <Col sm="2" style={{ marginTop: 10 }}>
+            <select>
+              <option name="15-mins">15 mins</option>
+              <option name="hour">1 hour</option>
+              <option name="today">Today</option>
+              <option name="past">Past</option>
+            </select>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
