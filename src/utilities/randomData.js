26faf1bfb5b70777b802cf7acc7500d9c57b4299
Kunagu Varun Kumar <kunaguvarun@gmail.com>
2019-07-10 18:55:59 -0400

    extract random generator as a function

--- src/utilities/randomData.js
+++ src/utilities/randomData.js
@@ -1,32 +1,37 @@
 import moment from "moment";
 moment().format();
-
-let startDate = new Date(moment().format("L"));
-let now = startDate;
 let stats = [];
-let servers = ["WACSRIB00001019", "WACSRIB00001020", "WACSRIB00001023"];
 
-while (moment(now).get("date") === moment(startDate).get("date")) {
-  // still today
-  now = moment(now)
-    .add(5, "m")
-    .toDate();
+const generateStatsFor = date => {
+  let startDate = new Date(moment(date).format("L"));
+  let now = startDate;
+  let servers = ["WACSRIB00001019", "WACSRIB00001020", "WACSRIB00001023"];
 
-  servers.forEach(s => {
-    stats.push({
-      server: s,
-      cpu:
-        moment(now).get("hour") > 20 && moment(now).get("hour") < 22
-          ? Math.floor(Math.random() * (100 - 80) + 1) + 80
-          : Math.floor(Math.random() * (10 - 1) + 1) + 1,
+  while (moment(now).get("date") === moment(startDate).get("date")) {
+    // still today
+    now = moment(now)
+      .add(5, "m")
+      .toDate();
 
-      memory: Math.floor(Math.random() * 100) + 1 + "%",
-      cDrive: Math.floor(Math.random() * 100) + 1 + "%",
-      dDrive: Math.floor(Math.random() * 100) + 1 + "%",
-      timestamp: now
+    servers.forEach(s => {
+      stats.push({
+        server: s,
+        cpu:
+          moment(now).get("hour") > 20 && moment(now).get("hour") < 22
+            ? Math.floor(Math.random() * (100 - 80) + 1) + 80
+            : Math.floor(Math.random() * (10 - 1) + 1) + 1,
+
+        memory: Math.floor(Math.random() * 100) + 1 + "%",
+        cDrive: Math.floor(Math.random() * 100) + 1 + "%",
+        dDrive: Math.floor(Math.random() * 100) + 1 + "%",
+        timestamp: now
+      });
     });
-  });
-}
+  }
+  return stats;
+};
+
+stats.concat(generateStatsFor("2019-07-10"), generateStatsFor("2019-07-09"));
 
 export const getCPU = duration => {
   let filteredStats = [];
@@ -51,6 +56,10 @@ export const getCPU = duration => {
       filteredStats = stats.filter(s =>
         moment(s.timestamp).isBetween(moment().format("L"), moment())
       );
+      break;
+
+    default:
+      filteredStats = stats.filter(s => moment(s.timestamp).isBefore(moment()));
   }
 
   const data = [
